a5cda21fd333f81352ef976cc4bcdb807b61b07c
Marcela Errazquin <merrazquin@gmail.com>
2018-02-24 14:33:53 -0500

    pseudocode for display and logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+/*
+    Create a list of possible puzzles
+    
+    Listen for a key to be pressed to start the game
+    
+    Randomize the list of puzzles
+
+    Select the fist puzzle to be solved.
+
+    Listen for keyboard input
+        if the user gueses a letter correctly, display that letter wherever it exists in the puzzle
+
+        if the user guesses incorrectly
+            add that letter to the "already guessed"
+            decrement number remaining guesses
+            display a new part of the hangman
+        
+        if the user wins or loses the game
+            increment win counter if applicable
+            restart game with new puzzle
+
+*/
+
+var game = {
+    
+}
