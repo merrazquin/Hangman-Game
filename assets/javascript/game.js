a4ba2dfa110061d1e1aac2f2f7350a06e13b0de1
Marcela Errazquin <merrazquin@gmail.com>
2018-02-24 15:16:33 -0500

    Basic game setup     - show spaces for selected word Basic styling for word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,6 +21,60 @@
 
 */
 
+// DOM elements
+var solutionDisplay = document.getElementById("solution");
+var invalidGuessesBank = document.getElementById("invalidGuesses");
+var remainingGuessesDisplay = document.getElementById("remainingGuesses");
+
 var game = {
-    
+    hasBegun: false,
+    answers: ["Jerry Seinfeld", "Elaine Benes", "Cosmo Kramer", "George Costanza", "black and white cookie", "chocolate babka", "muffin top", "serenity now", "frogger", "bottle wipe", "close talker", "double dipper", "Festivus", "hoochie mama", "manssiere", "Mulva", "David Puddy", "Jackie Chiles", "Tim Whatley"],
+    currentAnswer: "",
+    guessesRemaining: 10,
+    winCount: 0,
+    lossCount: 0,
+
+    restart: function () {
+        // reset guesses remaining
+        remainingGuessesDisplay.textContent = (this.guessesRemaining = 10); 
+
+        // randomize answer, select one, and put it back at the end of the list
+        this.answers.sort(randomize);
+        this.currentAnswer = this.answers.shift();
+        this.answers.push(this.currentAnswer);
+
+        var letters = this.currentAnswer.split("");
+        letters.forEach(letter => {
+            createNode(letter);
+        });
+    },
+
+    acceptGuess: function () {
+        
+    }
+
+}
+
+/* Helper Functions */
+function createNode(letter) {
+    var isSpace = letter == " ";
+    var node = document.createElement("li");
+    var textElement = document.createTextNode(isSpace ? "-" : " ");
+    if(!isSpace){
+        node.setAttribute("value", letter)
+        node.setAttribute("solved", "false");
+    }
+    else{      
+        node.setAttribute("solved", "true");
+        node.setAttribute("class", "space");
+    }
+
+    node.appendChild(textElement);
+    solutionDisplay.appendChild(node);
+
+}
+
+function randomize(a, b) {
+    var rand = Math.random();
+    return rand < .3 ? -1 : (rand < .6 ? 0 : 1);
 }
