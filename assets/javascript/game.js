3450e1b4e72be5bd0b26da07f7f9344a779bd9f1
Marcela Errazquin <merrazquin@gmail.com>
2018-02-25 12:29:55 -0500

    Refactoring all code back into game object
    Documentation
    
    reset renamed to newRound
    
    initial sound implementation
    
    Bootstrap implementation/preliminary styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,6 +27,14 @@ var invalidGuessesBank = document.getElementById("invalidGuesses");
 var remainingGuessesDisplay = document.getElementById("remainingGuesses");
 var winsDisplay = document.getElementById("roundsWon");
 var lossesDisplay = document.getElementById("roundsLost");
+var feedback = document.getElementById("feedback");
+
+// Sounds
+var introSound = new Audio("assets/sounds/intro.mp3");
+
+var loseSounds = [new Audio("assets/sounds/roundLoss.mp3"), new Audio("assets/sounds/roundLoss2.wav"), new Audio("assets/sounds/roundLoss3.mp3")];
+var winSounds = [new Audio("assets/sounds/roundWin.mp3"), new Audio("assets/sounds/roundWin2.wav"), new Audio("assets/sounds/roundWin3.wav")];
+
 
 var game = {
     hasBegun: false,
@@ -42,54 +50,102 @@ var game = {
     startGame: function () {
         this.hasBegun = true;
 
-        // randomize the answers
+        // introSound.play();
+
+        // randomize the answers and sounds
         this.answers.sort(randomize);
+        winSounds.sort(randomize);
+        loseSounds.sort(randomize);
+
+        feedback.innerHTML = "&nbsp;";
 
         // reset the game
-        this.reset();
+        this.newRound();
     },
-    reset: function () {
+    newRound: function () {
         // reset round status
         this.roundEnded = false;
 
         // reset guesses
         this.allGuesses = [];
         this.invalidGuesses = [];
-        updateRemainingGuesses(this.maxTries - this.invalidGuesses.length)
+
+        // update display of remaining guesses
+        remainingGuessesDisplay.textContent = this.maxTries - this.invalidGuesses.length;
 
         // select an answer, and put it back at the end of the list
         this.currentAnswer = this.answers.shift();
         this.answers.push(this.currentAnswer);
 
-        clearDOM();
+        // clear the solutions and the guess bank
+        while (solutionDisplay.firstChild) {
+            solutionDisplay.removeChild(solutionDisplay.firstChild);
+        }
+        while (invalidGuessesBank.firstChild) {
+            invalidGuessesBank.removeChild(invalidGuessesBank.firstChild);
+        }
 
+        // loop over each letter in the answer
         var letters = this.currentAnswer.split("");
         letters.forEach(letter => {
-            createNode(letter);
+            // create a letter space for the puzzle
+            var isSpace = letter == " ";
+            var node = document.createElement("li");
+            var textElement = document.createTextNode(isSpace ? "-" : "&nbsp;");
+
+            // treat spaces differently from letters
+            if (isSpace) {
+                node.setAttribute("solved", "true");
+            }
+            else {
+                node.setAttribute("value", letter.toLowerCase());
+                node.setAttribute("displayValue", letter);
+                node.setAttribute("solved", "false");
+            }
+            node.innerHTML = isSpace ? "-" : "&nbsp;";
+
+            // add the letter space to the collection (ul)
+            solutionDisplay.appendChild(node);
         });
     },
 
     acceptGuess: function (input) {
         console.log("accepted", input);
+
+        // check to see if leter has already been guessed
         if (this.allGuesses.indexOf(input) != -1) {
             console.log(input, "already guessed");
             return;
         }
 
+        // add current guess to all guesses
         this.allGuesses.push(input);
 
+        // collect nodes that match the input
         var correct = document.querySelectorAll('li[value="' + input + '"]');
+
+        // if there are no matching nodes, log an invalid guess
         if (!correct.length) {
             this.invalidGuesses.push(input);
-            addInvalidGuess(input);
-            updateRemainingGuesses(this.maxTries - this.invalidGuesses.length);
-        } else {
+
+            // add invalid guess to the bank
+            var node = document.createElement("li");
+            node.textContent = input;
+
+            invalidGuessesBank.appendChild(node);
+
+            // update display of remaining guesses
+            remainingGuessesDisplay.textContent = this.maxTries - this.invalidGuesses.length;
+        }
+        // otherwise, display the correct guess
+        else {
             correct.forEach(node => {
                 node.textContent = node.getAttribute("displayvalue");
                 node.setAttribute("solved", "true");
             });
         }
 
+        // check to see if round has either been won or lost
         if (document.querySelectorAll('li[solved="true"]').length === this.currentAnswer.length) {
             this.endRound(true);
         } else if (this.invalidGuesses.length === this.maxTries) {
@@ -97,74 +153,44 @@ var game = {
         }
     },
 
-    endRound: function(win) {
-        if(win) {
+    endRound: function (win) {
+        var sound;
+
+        if (win) {
             console.log("you won");
+
+            // select winning sound
+            sound = winSounds.shift();
+            winSounds.push(sound);
+
             this.winCount++;
         }
         else {
             console.log("you lost");
+
+            // var unsolved = document.querySelectorAll();
+
+            // select losing sound
+            sound = loseSounds.shift();
+            loseSounds.push(sound);
+
             this.lossCount++;
         }
 
-        updateRoundStats(this.winCount, this.lossCount);
+        feedback.textContent = (win ? "You won! " : "You lost! ") + "Press any key to play again.";
+
+        sound.play();
+
+        // update the scoreboard
+        winsDisplay.textContent = this.winCount;
+        lossesDisplay.textContent = this.lossCount;
+
         this.roundEnded = true;
     }
 }
 
-/* Helper Functions and DOM Manipulators */
-// clear the solutions and the guess bank
-function clearDOM() {
-    while (solutionDisplay.firstChild) {
-        solutionDisplay.removeChild(solutionDisplay.firstChild);
-    }
-    while (invalidGuessesBank.firstChild) {
-        invalidGuessesBank.removeChild(invalidGuessesBank.firstChild);
-    }
-}
+/* helper functions */
 
-// create a letter space for the puzzle
-function createNode(letter) {
-    var isSpace = letter == " ";
-    var node = document.createElement("li");
-    var textElement = document.createTextNode(isSpace ? "-" : "&nbsp;");
-    
-    // treat spaces differently from letters
-    if (isSpace) {
-        node.setAttribute("solved", "true");
-        node.setAttribute("class", "space");
-    }
-    else {
-        node.setAttribute("value", letter.toLowerCase());
-        node.setAttribute("displayValue", letter);
-        node.setAttribute("solved", "false");
-    }
-    node.innerHTML = isSpace ? "-" : "&nbsp;";
-
-    // node.appendChild(textElement);
-    solutionDisplay.appendChild(node);
-
-}
-
-// add invalid guess to the bank
-function addInvalidGuess(input) {
-    var node = document.createElement("li");
-    node.textContent = input;
-
-    invalidGuessesBank.appendChild(node);
-}
-
-// update display of remaining guesses
-function updateRemainingGuesses(remaining) {
-    remainingGuessesDisplay.textContent = remaining;
-}
-
-function updateRoundStats(wins, losses) {
-    winsDisplay.textContent = wins;
-    lossesDisplay.textContent = losses;
-}
-
-// helper function to randomize an array
 function randomize(a, b) {
     var rand = Math.random();
     return rand < .3 ? -1 : (rand < .6 ? 0 : 1);
@@ -178,7 +204,7 @@ document.onkeyup = function (event) {
     }
     // if the game has begun, but the round has ended, reset
     else if (game.roundEnded) {
-        game.reset();
+        game.newRound();
     }
     // otherwise, send the cleaned input to the game
     else {
